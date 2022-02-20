7f70ca8984bf78506ffa272feb7edf94e6468751
lucky-bots <94297859+lucky-bots@users.noreply.github.com>
2022-02-20 17:07:51 +0700

    Delete run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-# start the server - 
-# imports the app variable from the webapp subdirectory (package) 
-# and invokes its run method to start the server (app is an instance of Flask)
-
-from webapp import app
-app.run(debug=True)
